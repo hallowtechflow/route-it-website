76fd22f26793f04b3ce26b587d9097bf78954e26
Michael Henson <admin@hallowtech.us>
2026-07-29 02:34:47 -0400

    Initialize Route-it website

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <main className="home-page">
+            <h1>Route-it!</h1>
+            <p>Smarter routes. Less driving. More work completed.</p>
+        </main>
+    );
+}
+
+export default Home;
